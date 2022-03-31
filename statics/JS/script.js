d4263a8fd845b75a372907e9e71c9ac6d3d0a8bd
mitranikhil <mitranikhil93@gmail.com>
2022-03-31 22:54:30 +0530

    Initial commit

--- statics/JS/script.js
+++ statics/JS/script.js
@@ -21,67 +21,78 @@ searchMovies.addEventListener("input", () => {
 });
 
 // Function  to load contents
-const getPost = async()=>{
+const getPost = async()=> {
   const response = await fetch(`./statics/jsons/movie-api.json`)
   const data = await response.json();
   console.log(data);
-  data.items.map((data, index) =>{
+  data.items.map((data,
+    index) => {
     let html = "";
-    html += ` 
-              <div class="mb-3 text-light show-movie" id="movie-items">
-                <div class="row g-0">
-                  <div class="col-md-4">
-                    <img src="${data.image}" class="img-fluid rounded-start" alt="${data.movieTitle}" id="movie-image" />
-                  </div>
-                  <div class="col-md-8" id="card-content">
-                    <div class="card-body">
-                      <p class="title-txt visually-hidden">
-                        N0.${index+1} ${data.serchMovieTitle}
-                      </p>
-                      <h1 class="card-title">${data.movieTitle}</h1>
-                      <p id="movie-info">
-                        (${data.info})
-                      </p>
-                      <div>
-                        <p class="imdb-content">
-                          Rating: ${data.imdbRating} / 10 from ${data.imdbRatingCount} users
-                        </p>
-                        <p class="imdb-content">
-                          Metascore: ${data.metaScore}
-                        </p>
-                      </div>
-                      <hr />
-                      <p class="card-text">
-                        ${data.storyLineShort}
-                      </p>
-                      <p>
-                        Director: ${data.director}
-                      </p>
-                      <p>
-                        Writers: ${data.writers}
-                      </p>
-                      <p>
-                        Stars: ${data.stars}
-                      </p>
-                      <div id="download-link">
-                        <span class="d-inline-block" tabindex="0" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-content="Language: ${data.lan480p}"><a href="${data.down480p}" type="button" class="btn btn-outline-primary">480p - Download</a>
-                        </span>
-                        <span class="d-inline-block" tabindex="0" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-content="Language: ${data.lan720p}"><a href="${data.down720p}" type="button" class="btn btn-outline-primary">720p - Download</a>
-                        </span>
-                        <span class="d-inline-block" tabindex="0" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-content="Language: ${data.lan1080p}"><a href="${data.down1080p}" type="button" class="btn btn-outline-primary">1080p - Download</a>
-                        </span>
-                        <p class="card-text d-flex justify-content-end">
-                          <small class="text-muted">${data.lastUpdateDate}</small>
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              <hr class="bg-light" />
-              </div>
-            `;
-  let movieContent = document.getElementById("movie-content");
-  movieContent.insertAdjacentHTML("beforeend", html);
+    html += `
+    <div class="mb-3 text-light show-movie" id="movie-items">
+    <div class="row g-0">
+    <div class="col-md-4">
+    <img src="${data.image}" class="img-fluid rounded-start" alt="${data.movieTitle}" id="movie-image" />
+    </div>
+    <div class="col-md-8" id="card-content">
+    <div class="card-body">
+    <p class="title-txt visually-hidden">
+    N0.${index+1} ${data.serchMovieTitle}
+    </p>
+    <h1 class="card-title">${data.movieTitle}</h1>
+    <p id="movie-info">
+    (${data.info})
+    </p>
+    <div>
+    <p class="imdb-content">
+    Rating: ${data.imdbRating} / 10 from ${data.imdbRatingCount} users
+    </p>
+    <p class="imdb-content">
+    Metascore: ${data.metaScore}
+    </p>
+    </div>
+    <hr />
+    <p class="card-text">
+    ${data.storyLineShort}
+    </p>
+    <div class="mb-5 d-flex flex-column align-item-center justify-content-between" id="download-link">
+        <div class="btn-group my-2 w-50" role="group">
+          <button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">480p  
+          </button>
+          <div class="dropdown-menu bg-dark border-dark my-2" aria-labelledby="btnGroupDrop1">
+            <a type="button" target="_self" class="btn btn-outline-success"href="${data.down480p}">Download Link</a>
+            <div class="my-2 text-light">${data.lan480p}</div>
+          </div>
+        </div>
+        <div class="btn-group my-2 w-50" role="group">
+          <button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">720p  
+          </button>
+          <div class="dropdown-menu bg-dark border-dark my-2" aria-labelledby="btnGroupDrop1">
+            <a type="button" target="_self" class="btn btn-outline-success"href="${data.down720p}">Download Link</a>
+            <div class="my-2 text-light">${data.lan720p}</div>
+          </div>
+        </div>
+        <div class="btn-group my-2 w-50" role="group">
+          <button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">1080p
+          </button>
+          <div class="dropdown-menu bg-dark border-dark my-2" aria-labelledby="btnGroupDrop1">
+            <a type="button" target="_self" class="btn btn-outline-success"href="${data.down1080p}">Download Link</a>
+            <div class="my-2 text-light">${data.lan1080p}</div>
+          </div>
+        </div>
+    </div>
+    <p class="card-text d-flex justify-content-end">
+    <small class="text-muted">${data.lastUpdateDate}</small>
+    </p>
+    </div>
+    </div>
+    </div>
+    <hr class="bg-light" />
+    </div>
+    `;
+    let movieContent = document.getElementById("movie-content");
+    movieContent.insertAdjacentHTML("beforeend",
+      html);
   })
 }
-  getPost();
+getPost();
