227e85f4e192dd7e65089e3f4aaf0fe43405c795
mitranikhil <69292853+mitranikhil@users.noreply.github.com>
2022-10-16 14:51:17 +0530

    update script.js

--- statics/JS/script.js
+++ statics/JS/script.js
@@ -161,8 +161,8 @@ function show(data, index) {
               <button id="btnGroupDrop1" type="button" class="mx-1 btn btn-outline-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">480p
               </button>
               <div class="text-center dropdown-menu bg-light border-dark my-2 bgSize px-1" aria-labelledby="btnGroupDrop1">
-                <a type="button" target="_self" class="btn btn-danger" href="${data.uploadBaz480p}">Upload Baz</a>
-                <a type="button" target="_self" class="btn btn-info" href="${data.myBox480p}">My Box</a>
+                <a type="button" target="_blank" class="btn btn-danger" href="${data.uploadBaz480p}">Upload Baz</a>
+                <a type="button" target="_blank" class="btn btn-info" href="${data.myBox480p}">My Box</a>
                 <div class="my-2">
                   ${data.lan480p}
                 </div>
@@ -172,8 +172,8 @@ function show(data, index) {
               <button id="btnGroupDrop1" type="button" class="mx-1 btn btn-outline-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">720p
               </button>
               <div class="text-center dropdown-menu bg-light border-dark my-2 bgSize px-1" aria-labelledby="btnGroupDrop1">
-                <a type="button" target="_self" class="btn btn-danger" href="${data.uploadBaz720p}">Upload Baz</a>
-                <a type="button" target="_self" class="btn btn-info" href="${data.myBox720p}">My Box</a>
+                <a type="button" target="_blank" class="btn btn-danger" href="${data.uploadBaz720p}">Upload Baz</a>
+                <a type="button" target="_blank" class="btn btn-info" href="${data.myBox720p}">My Box</a>
                 <div class="my-2">
                   ${data.lan720p}
                 </div>
@@ -183,8 +183,8 @@ function show(data, index) {
               <button id="btnGroupDrop1" type="button" class="mx-1 btn btn-outline-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">1080p
               </button>
               <div class="text-center dropdown-menu bg-light border-dark my-2 bgSize px-1" aria-labelledby="btnGroupDrop1">
-                <a type="button" target="_self" class="btn btn-danger" href="${data.uploadBaz1080p}">Upload Baz</a>
-                <a type="button" target="_self" class="btn btn-info" href="${data.myBox1080p}">My Box</a>
+                <a type="button" target="_blank" class="btn btn-danger" href="${data.uploadBaz1080p}">Upload Baz</a>
+                <a type="button" target="_blank" class="btn btn-info" href="${data.myBox1080p}">My Box</a>
                 <div class="my-2">
                   ${data.lan1080p}
                 </div>
